68c6521b1c53d13482e62a3121acef993aa57950
Oghenetega Akpowenre <oghenetegaakpowenre@gmail.com>
2021-09-29 08:49:11 +0100

    initial commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:worldtime/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  String time = 'loading';
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: "Lagos", flag: "nigeria.png", url: "Africa/Lagos");
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitFoldingCube(
+          color: Colors.white,
+          size: 80.0,
+        ), //SpinKitRotatingCircle
+      ), //Center
+    ); //Scaffold
+  }
+}
